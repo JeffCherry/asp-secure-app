367bbb4e5d8195c9129a30fad749a38918a41f9b
Jeff Cherry <jeff.cherry92@gmail.com>
2016-08-20 20:55:37 -0400

    Add bootstrap and master page

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -4,12 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using NorthwindEFModel;
 
-public partial class Login : System.Web.UI.Page
+public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+
     }
 }
