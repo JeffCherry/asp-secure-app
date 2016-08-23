34d960f668cd0581c01defc640cb312326ad2e1b
Jeff Cherry <jeff.cherry92@gmail.com>
2016-08-22 23:23:08 -0400

    Implement login page

---
+++ App_Code/PageBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using NorthwindEFModel;
+
+public class PageBase : System.Web.UI.Page
+{
+    protected virtual void Page_Load(object sender, EventArgs e)
+    {
+        Master.FindControl("topNav").Visible = false;
+        Master.FindControl("sideNav").Visible = false;
+        LinkButton btnEmp = (LinkButton)Master.FindControl("btnEmployees");
+        LinkButton btnCust = (LinkButton)Master.FindControl("btnCustomers");
+        LinkButton btnOrd = (LinkButton)Master.FindControl("btnOrders");
+
+        if (Request.IsAuthenticated)
+        {
+          Master.FindControl("topNav").Visible = true;
+          Master.FindControl("sideNav").Visible = true;
+            NorthwindEntities ne = new NorthwindEntities();
+            int id = int.Parse(User.Identity.Name);
+            Employee emp = (from em in ne.Employees
+                            where em.EmployeeID == id
+                            select em).FirstOrDefault<Employee>();
+            Label lblIdentity = (Label)Master.FindControl("lblWhoAmI");
+            lblIdentity.Text = emp.FirstName + " " + emp.LastName; 
+
+        }
+        if (User.IsInRole("Admin"))
+        { 
+            btnEmp.Visible = true;
+            btnCust.Visible = true;
+            btnOrd.Visible = true;
+        }
+        if (User.IsInRole("User"))
+        {
+            btnEmp.Visible = false;
+            btnCust.Visible = true;
+            btnOrd.Visible = true;
+        }
+    }
+}
--- Login.aspx.cs
+++ Login.aspx.cs
@@ -4,11 +4,44 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using NorthwindEFModel;
 
-public partial class Login : System.Web.UI.Page
+public partial class Login : PageBase
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected override void Page_Load(object sender, EventArgs e)
     {
+        base.Page_Load(sender, e);
 
+    }
+
+    protected void lgin_Authenticate(object s, AuthenticateEventArgs e)
+    {
+        string useName = lgnApp.UserName;
+        string password = lgnApp.Password;
+
+        using (NorthwindEntities entityContext = new NorthwindEntities())
+        {
+            Employee em = (from emp in entityContext.Employees
+                           where emp.LastName == useName.ToUpper()
+                           && emp.PostalCode.Trim() == password
+                           select emp).FirstOrDefault<Employee>();
+            if (em != null)
+            {
+                FormsAuthentication.RedirectFromLoginPage(em.EmployeeID.ToString(),
+                lgnApp.RememberMeSet);
+
+                string returnUrl = Request.QueryString["Welcome/Landing.aspx"];
+
+                // check if it exists, if not then redirect to default page
+                if (returnUrl == null) returnUrl = "~/Login.aspx";
+                Response.Redirect(returnUrl);
+            }
+        }
+    }
+
+    protected void Unnamed_PreRender(object sender, EventArgs e)
+    {
+        Response.Redirect("Welcome/Landing.aspx");
     }
 }
